d3f51c1749f98414f9c1ba9c4721b796b96f4512
Chris S <mrshrinkray@gmail.com>
2017-02-18 11:17:21 +0000

    Added babel.js and some test scripts

--- src/Letmein.Web/wwwroot/_references.js
+++ src/Letmein.Web/wwwroot/_references.js
@@ -3,4 +3,6 @@
 /// <reference path="js/jquery.countdown.min.js" />
 /// <reference path="js/jquery.toast.js" />
 /// <reference path="js/jquery-3.1.1.min.js" />
+/// <reference path="js/js/letmein-test.js" />
+/// <reference path="js/letmein-test.js" />
 /// <reference path="js/sjcl.js" />
---
+++ src/Letmein.Web/wwwroot/js/letmein.js
@@ -0,0 +1,129 @@
+﻿class IndexView {
+
+	constructor($, window) {
+		console.log($);
+		$(document).ready(function () {
+			$("#text-textarea").focus();
+
+			$("#encrypt-form").submit(function () {
+				if ($("#text-textarea").val() === "") {
+					showError("Please enter some text.");
+					return false;
+				}
+
+				if ($("#password-input").val().length < 5) {
+					showError("Please enter a password 5 or more characters long.");
+					return false;
+				}
+
+				var text = $("#text-textarea").val();
+				var password = $("#password-input").val();
+
+				// Clear so they're not POST'd
+				$("#text-textarea").val("");
+				$("#password-input").val("");
+
+				// Encrypt
+				var json = window.sjcl.encrypt(password, text);
+				$("#cipherJson").val(json);
+
+				return true;
+			});
+		});
+	}
+
+	showError(text) {
+		$.toast({
+			heading: "Error",
+			text: text,
+			icon: "error",
+			showHideTransition: "fade",
+			allowToastClose: true,
+			hideAfter: 2000,
+			stack: 5,
+			position: "top-center",
+			textAlign: "left",
+			loader: false
+		});
+	}
+}
+
+var StoreView = (function ($) {
+	$(document).ready(function () {
+		var clipboard = new Clipboard("#copy-link");
+		clipboard.on("success", function (e) {
+			$.toast({
+				text: "Copied to clipboard",
+				heading: "Success",
+				icon: "information",
+				showHideTransition: "fade",
+				allowToastClose: true,
+				hideAfter: 2000,
+				stack: 5,
+				position: "top-center",
+				textAlign: "left",
+				loader: false
+			});
+		});
+	});
+})();
+
+var LoadView = (function ($, expiry) {
+	$(document).ready(function () {
+		$("#password-input").focus();
+
+		$("#decrypt-form").submit(function () {
+			return false;
+		});
+
+		$("#decrypt-button").click(function () {
+			var cipherJson = $("#cipherJson").val();
+			var password = $("#password-input").val();
+
+			try {
+				var text = window.sjcl.decrypt(password, cipherJson);
+				$("#cipher-textarea").val(text);
+
+				$("#expiry-date").show();
+				$("#expiry-date")
+					.on("finish.countdown",
+						function () {
+							document.location.reload();
+						})
+					.countdown(expiry, function (event) {
+						$(this).text(
+							event.strftime('Expires in %D days %H hours %M mins %S secs')
+						);
+					});
+
+				$.toast({
+					heading: "Success",
+					text: "Your note was decrypted.",
+					icon: "success",
+					showHideTransition: "fade",
+					allowToastClose: true,
+					hideAfter: 2000,
+					stack: 5,
+					position: "top-center",
+					textAlign: "left",
+					loader: false
+				});
+			} catch (err) {
+				$.toast({
+					heading: "Failure",
+					text: "Unable to decrypt the note.",
+					icon: "error",
+					showHideTransition: "fade",
+					allowToastClose: true,
+					hideAfter: 2000,
+					stack: 5,
+					position: "top-center",
+					textAlign: "left",
+					loader: false
+				});
+
+				console.log(err);
+			}
+		});
+	});
+})();
