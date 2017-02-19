63d29da7fcebdea7d79a68b97838c8c941441df8
Chris S <mrshrinkray@gmail.com>
2017-02-19 19:43:13 +0000

    prepare-wwwroot.sh paths

--- travis/prepare-wwwroot.sh
+++ travis/prepare-wwwroot.sh
@@ -1,9 +1,9 @@
-./node_modules/.bin/babel js/letmein.js -o js/prod/letmein.js
-./node_modules/.bin/uglifyjs js/prod/letmein.js js/libraries/*.js > js/prod/letmein.min.js
+./node_modules/.bin/babel ./src/Letmein.Web/wwwroot/js/letmein.js -o ./src/Letmein.Web/wwwroot/js/prod/letmein.js
+./node_modules/.bin/uglifyjs ./src/Letmein.Web/wwwroot/js/prod/letmein.js js/libraries/*.js > ./src/Letmein.Web/wwwroot/js/prod/letmein.min.js
 
-rm -rf src/Letmein.Web/wwwroot/js/libraries
-rm src/Letmein.Web/wwwroot/js/letmein.js
-rm src/Letmein.Web/wwwroot/.eslintrc.json
-rm src/Letmein.Web/wwwroot/js/_references.js
-rm src/Letmein.Web/wwwroot/js/.babelrc
-rm src/Letmein.Web/wwwroot/js/package.json
+rm -rf ./src/Letmein.Web/wwwroot/js/libraries
+rm ./src/Letmein.Web/wwwroot/js/letmein.js
+rm ./src/Letmein.Web/wwwroot/.eslintrc.json
+rm ./src/Letmein.Web/wwwroot/js/_references.js
+rm ./src/Letmein.Web/wwwroot/js/.babelrc
+rm ./src/Letmein.Web/wwwroot/js/package.json
