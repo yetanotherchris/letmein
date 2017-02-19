fe8b509d8cbeb5669451b5562df21b80e51d8916
Chris S <mrshrinkray@gmail.com>
2017-02-19 18:35:00 +0000

    Use minifitjs to combine files, travis.yml file builds JS now

--- src/Letmein.Web/wwwroot/js/letmein.js
+++ src/Letmein.Web/wwwroot/js/letmein.js
@@ -1,18 +1,26 @@
-﻿class IndexView {
+﻿// npm install eslint --save-dev
+// ./node_modules/.bin/eslint js/letmein-babel.js
+
+// npm install --save-dev uglify-js
+// npm install --save-dev babel-cli
+// npm install babel-preset-env --save-dev
+// ./node_modules/.bin/babel js/letmein.js -o js/prod/letmein.js
+// ./node_modules/.bin/uglifyjs js/prod/letmein.js js/libraries/*.js > js/prod/letmein.min.js
+class IndexView {
 
 	constructor($, window) {
-		console.log($);
-		$(document).ready(function () {
+
+		$(document).ready(() => {
 			$("#text-textarea").focus();
 
-			$("#encrypt-form").submit(function () {
+			$("#encrypt-form").submit(() => {
 				if ($("#text-textarea").val() === "") {
-					showError("Please enter some text.");
+					this.showError($, "Please enter some text.");
 					return false;
 				}
 
 				if ($("#password-input").val().length < 5) {
-					showError("Please enter a password 5 or more characters long.");
+					this.showError($, "Please enter a password 5 or more characters long.");
 					return false;
 				}
 
@@ -32,7 +40,7 @@
 		});
 	}
 
-	showError(text) {
+	showError($, text) {
 		$.toast({
 			heading: "Error",
 			text: text,
@@ -48,82 +56,88 @@
 	}
 }
 
-var StoreView = (function ($) {
-	$(document).ready(function () {
-		var clipboard = new Clipboard("#copy-link");
-		clipboard.on("success", function (e) {
-			$.toast({
-				text: "Copied to clipboard",
-				heading: "Success",
-				icon: "information",
-				showHideTransition: "fade",
-				allowToastClose: true,
-				hideAfter: 2000,
-				stack: 5,
-				position: "top-center",
-				textAlign: "left",
-				loader: false
+class StoreView {
+
+	constructor($) {
+		$(document).ready(() => {
+			var clipboard = new Clipboard("#copy-link");
+			clipboard.on("success", (e) => {
+				$.toast({
+					text: "Copied to clipboard",
+					heading: "Success",
+					icon: "information",
+					showHideTransition: "fade",
+					allowToastClose: true,
+					hideAfter: 2000,
+					stack: 5,
+					position: "top-center",
+					textAlign: "left",
+					loader: false
+				});
 			});
 		});
-	});
-})();
+	}
+}
 
-var LoadView = (function ($, expiry) {
-	$(document).ready(function () {
-		$("#password-input").focus();
+class LoadView {
 
-		$("#decrypt-form").submit(function () {
-			return false;
-		});
+	constructor(window, $, expiry) {
+		$(document).ready(() => {
+			$("#password-input").focus();
 
-		$("#decrypt-button").click(function () {
-			var cipherJson = $("#cipherJson").val();
-			var password = $("#password-input").val();
+			$("#decrypt-form").submit(() => {
+				return false;
+			});
 
-			try {
-				var text = window.sjcl.decrypt(password, cipherJson);
-				$("#cipher-textarea").val(text);
+			$("#decrypt-button").click(() => {
+				var cipherJson = $("#cipherJson").val();
+				var password = $("#password-input").val();
 
-				$("#expiry-date").show();
-				$("#expiry-date")
-					.on("finish.countdown",
+				try {
+					var text = window.sjcl.decrypt(password, cipherJson);
+					$("#cipher-textarea").val(text);
+
+					$("#expiry-date").show();
+					$("#expiry-date")
+						.on("finish.countdown",
 						function () {
 							document.location.reload();
 						})
-					.countdown(expiry, function (event) {
-						$(this).text(
-							event.strftime('Expires in %D days %H hours %M mins %S secs')
-						);
+						.countdown(expiry, function (event) {
+							$(this).text(
+								event.strftime("Expires in %D days %H hours %M mins %S secs")
+							);
+						});
+
+					$.toast({
+						heading: "Success",
+						text: "Your note was decrypted.",
+						icon: "success",
+						showHideTransition: "fade",
+						allowToastClose: true,
+						hideAfter: 2000,
+						stack: 5,
+						position: "top-center",
+						textAlign: "left",
+						loader: false
+					});
+				} catch (err) {
+					$.toast({
+						heading: "Failure",
+						text: "Unable to decrypt the note.",
+						icon: "error",
+						showHideTransition: "fade",
+						allowToastClose: true,
+						hideAfter: 2000,
+						stack: 5,
+						position: "top-center",
+						textAlign: "left",
+						loader: false
 					});
 
-				$.toast({
-					heading: "Success",
-					text: "Your note was decrypted.",
-					icon: "success",
-					showHideTransition: "fade",
-					allowToastClose: true,
-					hideAfter: 2000,
-					stack: 5,
-					position: "top-center",
-					textAlign: "left",
-					loader: false
-				});
-			} catch (err) {
-				$.toast({
-					heading: "Failure",
-					text: "Unable to decrypt the note.",
-					icon: "error",
-					showHideTransition: "fade",
-					allowToastClose: true,
-					hideAfter: 2000,
-					stack: 5,
-					position: "top-center",
-					textAlign: "left",
-					loader: false
-				});
-
-				console.log(err);
-			}
+					console.log(err);
+				}
+			});
 		});
-	});
-})();
+	}
+}
