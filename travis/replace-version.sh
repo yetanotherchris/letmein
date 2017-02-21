72fe487aff2eb3a0ab00d89675e2e90642377488
Chris S <mrshrinkray@gmail.com>
2017-02-21 15:17:35 +0000

    version replace script was using sed wrong

--- travis/replace-version.sh
+++ travis/replace-version.sh
@@ -1,2 +1,2 @@
-sed '|AssemblyVersion\("1\.0\.\*"\)|$DOCKER_TAG' src/Letmein.Web/Properties/AssemblyInfo.cs > src/Letmein.Web/Properties/AssemblyInfo.cs
+sed 's|AssemblyVersion\("1\.0\.\*"\)|$DOCKER_TAG' src/Letmein.Web/Properties/AssemblyInfo.cs > src/Letmein.Web/Properties/AssemblyInfo.cs
 more src/Letmein.Web/Properties/AssemblyInfo.cs
