b016a6b8935e86aaa3f836b4e8e0b51a2809d368
Chris S <mrshrinkray@gmail.com>
2017-02-21 16:49:06 +0000

    Add src files in gulp

--- src/Letmein.Web/wwwroot/gulpfile.js
+++ src/Letmein.Web/wwwroot/gulpfile.js
@@ -9,6 +9,8 @@ const rename = require('gulp-rename');
 gulp.task('default', ['babel', 'concat-uglify']);
 
 gulp.task('babel', () => {
+
+	// Use babeljs on the letmein javascript to transcode it from ECMAScript
 	return gulp
 		.src('js/letmein.js')
 		.pipe(babel({
@@ -19,9 +21,16 @@ gulp.task('babel', () => {
 
 gulp.task('concat-uglify', () => {
 	return gulp
-		.src(['js/prod/letmein.js'])
-		.pipe(concat('js/libraries/*.js'))
+		.src(['js/libraries/jquery-3.1.1.min.js',
+			'js/libraries/bootstrap.min.js',
+			'js/libraries/bootbox.min.js',
+			'js/libraries/clipboard.min.js',
+			'js/libraries/jquery.countdown.min.js',
+			'js/libraries/jquery.toast.js',
+			'js/libraries/sjcl.js',
+			'js/prod/letmein.js'
+		])
+		.pipe(concat('letmein.min.js'))
 		.pipe(uglify())
-		.pipe(rename('letmein.min.js'))
 		.pipe(gulp.dest('js/prod'));
 });
