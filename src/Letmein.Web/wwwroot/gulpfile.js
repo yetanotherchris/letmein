a00bec1f1ebe04b4e38439436dab37815c46d286
Chris S <mrshrinkray@gmail.com>
2017-02-23 16:43:01 +0000

    Fix for javascript version number in Gulp

--- src/Letmein.Web/wwwroot/gulpfile.js
+++ src/Letmein.Web/wwwroot/gulpfile.js
@@ -5,6 +5,8 @@ const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
 const rename = require('gulp-rename');
+const argv = require('yargs').argv;
+var gutil = require('gulp-util');
 
 gulp.task('default', ['babel', 'concat-uglify']);
 
@@ -20,6 +22,10 @@ gulp.task('babel', () => {
 });
 
 gulp.task('concat-uglify', () => {
+
+	var destFilename = `letmein.${argv.docker_tag}.min.js`;
+	gutil.log(`Filename: "${destFilename}"`);
+
 	return gulp
 		.src(['js/libraries/jquery-3.1.1.min.js',
 			'js/libraries/bootstrap.min.js',
@@ -30,7 +36,7 @@ gulp.task('concat-uglify', () => {
 			'js/libraries/sjcl.js',
 			'js/prod/letmein.js'
 		])
-		.pipe(concat('letmein.min.js'))
+		.pipe(concat(destFilename))
 		.pipe(uglify())
 		.pipe(gulp.dest('js/prod'));
 });
