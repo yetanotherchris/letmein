a605a32f1f9c13b4650fa7e1146a925161cbbd63
Chris S <mrshrinkray@gmail.com>
2017-02-27 23:38:03 +0000

    Make the pull requests run a docker build

--- travis/run-pullrequest.sh
+++ travis/run-pullrequest.sh
@@ -11,3 +11,8 @@ chmod 777 ./travis/clean-wwwroot.sh
 dotnet restore
 cd src/Letmein.Tests/
 dotnet test
+cd ../../
+dotnet publish src/Letmein.Web -c Release
+cd src/Letmein.Web
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker build -t "anotherchris/letmein:latest" -t "anotherchris/letmein:$DOCKER_TAG" .
