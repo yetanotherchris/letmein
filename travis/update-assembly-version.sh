c6b467c9530495141c183d9de9f36d5b13e748e0
Chris S <chrisasmall+github@gmail.com>
2018-09-10 18:13:42 +0100

    travis: increment version no.

--- travis/update-assembly-version.sh
+++ travis/update-assembly-version.sh
@@ -1,1 +1,3 @@
-# empty
+sed "s|<Version>99.88.77</Version>|<Version>$DOCKER_TAG</Version>|" src/Letmein.Web/Letmein.Web.csproj > src/Letmein.Web/Letmein.Web.csproj.temp
+mv src/Letmein.Web/Letmein.Web.csproj.temp src/Letmein.Web/Letmein.Web.csproj
+cat src/Letmein.Web/Letmein.Web.csproj
